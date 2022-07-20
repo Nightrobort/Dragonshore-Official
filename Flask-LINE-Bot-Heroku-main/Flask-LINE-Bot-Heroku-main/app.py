a2943f986b3abdc1e73e1cd8542150fb22922abf
Nightrobort <106804328+Nightrobort@users.noreply.github.com>
2022-07-20 21:51:19 +0800

    Update app.py

--- Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
+++ Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
@@ -62,9 +62,7 @@ def handle_message(event):
     str1 = ""
 
     # Send To Line
-    aa = str(number22)
-    reply = TextSendMessage("所以您是想" + StringforAction + StringforBrand + "的" + StringforDevice + "是嗎?"+ aa)
-    number22 += 1
+    reply = TextSendMessage("所以您是想" + StringforAction + StringforBrand + "的" + StringforDevice + "是嗎?")
     # reply = TextSendMessage(text=f"{get_message}")  原版 : 讀取訊息後回復一樣的訊息
     line_bot_api.reply_message(event.reply_token, reply)
 
