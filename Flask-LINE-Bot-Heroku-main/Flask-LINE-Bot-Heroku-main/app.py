ddeb7d78fb433907303277326d3249fed25332b6
Nightrobort <106804328+Nightrobort@users.noreply.github.com>
2022-07-20 20:56:41 +0800

    Update app.py

--- Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
+++ Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
@@ -30,11 +30,28 @@ def callback():
 
         return "OK"
 
+StringforDevice = ""
+StringforAction = ""
+StringforBrand = ""
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
+    splitString(get_message)
 
     # Send To Line
-    reply = TextSendMessage("成功 @@")
+    reply = TextSendMessage("所以您是想" + StringforAction + StringforBrand + "的" + StringforDevice + "是嗎?")
+    # reply = TextSendMessage(text=f"{get_message}")  原版 : 讀取訊息後回復一樣的訊息
     line_bot_api.reply_message(event.reply_token, reply)
+
+def splitString(string_):
+    str1 = string_.split("充電機")
+    if len(str1) > 1:
+        StringforDevice = "充電機"
+    str1 = string_.split("買")
+    if len(str1) > 1:
+        StringforAction = "買"
+    str1 = string_.split("Mastervolt")
+    if len(str1) > 1:
+        StringforBrand = "Mastervolt"
