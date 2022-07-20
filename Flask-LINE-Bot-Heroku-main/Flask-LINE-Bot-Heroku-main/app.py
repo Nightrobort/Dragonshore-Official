3090d1f47a99015ced3c2d1ebd86ef411e2be0d8
Nightrobort <106804328+Nightrobort@users.noreply.github.com>
2022-07-20 21:32:25 +0800

    Update app.py

--- Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
+++ Flask-LINE-Bot-Heroku-main/Flask-LINE-Bot-Heroku-main/app.py
@@ -39,20 +39,8 @@ hollew = "123"
     
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    def splitString(string_):
-        hollew = "執行成功"
-        str1 = string_.split("充電機")
-        if (len(str1) > 1):
-            StringforDevice = "充電機"
-        str1 = ""
-        str1 = string_.split("買")
-        if (len(str1) > 1):
-            StringforAction = "買"
-        str1 = ""
-        str1 = string_.split("Mastervolt")
-        if (len(str1) > 1):
-            StringforBrand = "Mastervolt"
-        str1 = ""
+
+
     get_message = event.message.text
     splitString(get_message)
 
@@ -61,4 +49,5 @@ def handle_message(event):
     # reply = TextSendMessage(text=f"{get_message}")  原版 : 讀取訊息後回復一樣的訊息
     line_bot_api.reply_message(event.reply_token, reply)
 
-
+def splitString():
+    hollew = "執行成功"
